921cce5cee73cebd7aa08daba2fa271d38258b43
lynnofthewired <s.lynn.makowski@gmail.com>
2023-06-05 09:58:18 -0400

    more done, no errors, but not everything works

--- content_merge_bot.py
+++ content_merge_bot.py
@@ -2,15 +2,37 @@ import discord
 import os
 import json
 from discord.ext import commands
+from discord import PermissionOverwrite, Permissions
 
 intents = discord.Intents.default()
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
 intents.guilds = True
 intents.messages = True
+intents.message_content = True
+
+@bot.command()
+async def merge(ctx, channel1: discord.TextChannel, channel2: discord.TextChannel):
+    overwrites = {
+        ctx.guild.default_role: PermissionOverwrite(read_messages=False),
+        ctx.author: PermissionOverwrite(read_messages=True),
+        bot.user: PermissionOverwrite(read_messages=True)
+    }
+
+    new_channel = await ctx.guild.create_text_channel(
+        f'merged-{channel1.name}-{channel2.name}', 
+        overwrites=overwrites
+    )
+
+    async for message in channel1.history(oldest_first=True):
+        await new_channel.send(f"**{message.author.name} at {message.created_at} said**:\n{message.content}")
+
+    async for message in channel2.history(oldest_first=True):
+        await new_channel.send(f"**{message.author.name} at {message.created_at} said**:\n{message.content}")
 
 with open('config.json') as config_file:
     config = json.load(config_file)
-
-bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -20,26 +42,36 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('pong')
 
-@bot.command()
-async def merge(ctx, channel1: discord.TextChannel, channel2: discord.TextChannel):
-    # This function is a command for the bot that will merge the contents of two channels into a new one
-
-    # The command takes three arguments:
-    # - ctx: The context in which the command was called. This contains information like the message that called the command and the server (guild) in which it was called
-    # - channel1 and channel2: These are the two channels that you want to merge. They are of type discord.TextChannel, which means the command expects them to be text channels.
-
-    # First, the bot creates a new channel in the same server (guild) the command was called in
-    # The name of the new channel is "merged-" followed by the names of the two channels being merged
-    new_channel = await ctx.guild.create_text_channel(f'merged-{channel1.name}-{channel2.name}')
-
-    # Then, the bot goes through the message history of the first channel from oldest to newest
-    async for message in channel1.history(oldest_first=True):
-        # For each message, it sends a new message in the new channel
-        # This message contains the name of the author of the original message, the time it was sent, and its content
-        await new_channel.send(f"{message.author.name} at {message.created_at} said: {message.content}")
-
-    # The bot does the same for the second channel
-    async for message in channel2.history(oldest_first=True):
-        await new_channel.send(f"{message.author.name} at {message.created_at} said: {message.content}")
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(f'An error occurred: {str(error)}')
 
 bot.run(config['token'])
+
+@bot.command()
+async def merge(ctx, channel1: discord.TextChannel, channel2: discord.TextChannel):
+    overwrites = {
+        ctx.guild.default_role: PermissionOverwrite(read_messages=False),
+        ctx.author: PermissionOverwrite(read_messages=True),
+        bot.user: PermissionOverwrite(read_messages=True)
+    }
+
+    new_channel = await ctx.guild.create_text_channel(
+        f'merged-{channel1.name}-{channel2.name}', 
+        overwrites=overwrites
+    )
+
+    for channel in [channel1, channel2]:
+        async for message in channel.history(oldest_first=True):
+            content = f"{message.author.name} at {message.created_at} said:\n{message.content}"
+            files = await process_attachments(message.attachments)
+            await new_channel.send(content=content, files=files)
+
+async def process_attachments(attachments):
+    files = []
+    for attachment in attachments:
+        # Download the attachment to a file in memory
+        fp = io.BytesIO()
+        await attachment.save(fp)
+        files.append(discord.File(fp, filename=attachment.filename))
+    return files
